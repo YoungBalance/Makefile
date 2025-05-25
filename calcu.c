da83b592f2a51cf69e68385768e719d3abc82e80
Zhaohaiyang <Young_Balance@163.com>
2025-05-25 13:57:42 +0800

    Initialization code

---
+++ calcu.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "calcu.h"
+
+int calcu(int a, int b)
+{
+    return a + b;
+}
---
+++ input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "input.h"
+
+void input_int(int* a, int *b)
+{
+    printf("please input two nums");
+    scanf("%d %d",a,b);
+    printf("\r\n");
+}
